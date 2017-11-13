8b39f4ee5ab295e49a4e0729ccc51e356a2e7cd0
vovmanool <v.ropalo@gmail.com>
2017-11-13 12:36:10 +0200

    perestroika

--- app/src/main/java/com/vvv/manool/bee/Main2Activity.java
+++ app/src/main/java/com/vvv/manool/bee/Main2Activity.java
@@ -2,6 +2,8 @@ package com.vvv.manool.bee;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class Main2Activity extends Activity {
 
@@ -9,5 +11,13 @@ public class Main2Activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_main3,menu);
+
+        return true;
     }
 }
